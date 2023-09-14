e76ed3c6fb760186cde0530a5a498a26a4bb8726
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-14 13:46:37 -0300

    entering user data - part 2

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import { Container, Header, Avatar, Login, Name } from './styles'
+import { MdGroup, MdLocationCity, MdWork, MdLink } from "react-icons/md";
+
+import { Container, Header, Avatar, Login, Name, Inner, Data } from "./styles";
 
 function Profile() {
   return (
     <Container>
       <Header>
         <Avatar src="https://avatars.githubusercontent.com/u/84151293?v=4" />
-          <Login>filipe-alves-oliveira</Login>
-          <Name>Filipe Alves de Oliveira</Name>
+        <Login>filipe-alves-oliveira</Login>
+        <Name>Filipe Alves de Oliveira</Name>
       </Header>
+      <Inner>
+        <Data>
+          <MdGroup size={20} />
+          30 seguidores - 10 seguindo
+        </Data>
+        <Data>
+          <MdWork size={20} />
+          Opah
+        </Data>
+        <Data>
+          <MdLocationCity size={20} />
+          Cândido Mota - SP
+        </Data>
+        <Data>
+          <MdLink size={20} /> &nbsp;
+          <a href="https://opah.com.br">www.opah.com.br</a>
+        </Data>
+      </Inner>
     </Container>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
