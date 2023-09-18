186155ae9dc7b64b525729335ddd055f8de068a0
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-18 16:37:28 -0300

    crating service function getLangsFrom

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,32 @@
 import axios from "axios";
+import { langColor } from "./config";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
+
+export const getLangsFrom = (repositories) => {
+  let stats = repositories
+    .map((repository) => repository.language)
+    .reduce(
+      (data, language) => ({
+        ...data,
+        [language]: (data[language] || 0) + 1,
+      }),
+      []
+    );
+  delete stats.null;
+
+  // criar um array com todas as keys, linguagens q temos
+  stats = Object.keys(stats)
+    .map((language) => ({
+      name: language,
+      count: stats[language],
+      color: langColor[language.toLowerCase()],
+    }))
+    .sort((a, b) => b.count - a.count);
+
+    return stats
+}
