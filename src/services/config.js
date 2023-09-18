a9943f91c79d11e70dc002b05cd808c6197cd76c
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-18 16:37:04 -0300

    crating service config

---
+++ src/services/config.js
@@ -0,0 +1,14 @@
+export const langColor = {
+    javascript: "#f1c40f",
+    typescript: "#2980b9",
+    python: "#2ecc71",
+    html: "#e67e22",
+    css: "#9b59b6",
+    java: "#9b59b3",
+    vue: "#1abc9c",
+    php: "#3498db",
+    go: "#72aed8",
+    ruby: "red",
+    c: "black",
+    "c++": "gray",
+}
