cd0dfc53c99f55172a21244f8d9360c6536695ea
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-18 13:16:23 -0300

    creating user array with API information and using PropTypes

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,37 +1,51 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import { MdGroup, MdLocationCity, MdWork, MdLink } from "react-icons/md";
 
 import { Container, Header, Avatar, Login, Name, Inner, Data } from "./styles";
 
-function Profile() {
+function Profile({ user }) {
   return (
     <Container>
       <Header>
-        <Avatar src="https://avatars.githubusercontent.com/u/84151293?v=4" />
-        <Login>filipe-alves-oliveira</Login>
-        <Name>Filipe Alves de Oliveira</Name>
+        <Avatar src={user.avatar_url} />
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
       </Header>
       <Inner>
         <Data>
           <MdGroup size={20} />
-          30 seguidores - 10 seguindo
+          {user.following} seguidores - {user.followers} seguindo
         </Data>
         <Data>
           <MdWork size={20} />
-          Opah
+          {user.company} - &nbsp;
+          <a href="https://opah.com.br">https://opah.com.br/ </a>
         </Data>
         <Data>
           <MdLocationCity size={20} />
-          Cândido Mota - SP
+          {user.location}
         </Data>
         <Data>
           <MdLink size={20} /> &nbsp;
-          <a href="https://opah.com.br">www.opah.com.br</a>
+          <a href={user.blog}>{user.blog}</a>
         </Data>
       </Inner>
     </Container>
   );
 }
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
 
 export default Profile;
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -7,10 +7,21 @@ import Repositories from "./Repositories";
 import { Container, Sidebar, Main } from "./styles";
 
 function RepositoriesPage() {
+  const user = {
+    login: "filipe-alves-oliveira",
+    name: "Filipe Alves de Oliveira",
+    avatar_url: "https://avatars.githubusercontent.com/u/84151293?v=4",
+    followers: 6,
+    following: 9,
+    company: "Opah IT",
+    blog: "https://github.com/filipe-alves-oliveira",
+    location: "Cândido Mota - SP",
+  };
+
   return (
     <Container>
       <Sidebar>
-        <Profile />
+        <Profile user={user} />
         <Filter />
       </Sidebar>
       <Main>
