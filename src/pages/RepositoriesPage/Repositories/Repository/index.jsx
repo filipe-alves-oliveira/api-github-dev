2db19d84e3fe1127d5cc6de01d48341447011fcd
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-19 21:55:50 -0300

    fix folder repository

--- src/pages/RepositoriesPage/Repositories/Repository/index.jsx
+++ src/pages/RepositoriesPage/Repositories/Repository/index.jsx
@@ -5,28 +5,26 @@ import { Container, Name, Description, Footer, Lang, Link } from "./styles";
 
 function Repository({ repository }) {
   return (
-      <Container color="blue">
-        <Name>{repository.name}</Name>
-        <Description>{repository.description}</Description>
-        <Footer color="red">
-            <Lang>{repository.language}</Lang>
-            <Link href={repository.html_url} target="_blank">Ver</Link>
-        </Footer>
-      </Container>
+    <Container color="blue">
+      <Name>{repository.name}</Name>
+      <Description>{repository.description}</Description>
+      <Footer color="red">
+        <Lang>{repository.language}</Lang>
+        <Link href={repository.html_url} target="_blank">
+          Ver
+        </Link>
+      </Footer>
+    </Container>
   );
 }
 
-Repositories.propTypes = {
-  languages: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      html_url: PropTypes.string.isRequired,
-      language: PropTypes.string,
-    }).isRequired
-  ).isRequired,
+Repository.propTypes = {
+  repository: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    language: PropTypes.string,
+    html_url: PropTypes.string.isRequired,
+  }).isRequired,
 };
-
 
 export default Repository;
